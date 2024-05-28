81c8c772dd0a59fa9039193dd4dca022a8197057
yanjike <2711396853@qq.com>
2024-05-28 16:08:31 +0800

    redux删除

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -7,20 +7,16 @@ import zhCN from 'antd/locale/zh_CN';
 import './index.css';
 import 'dayjs/locale/zh-cn';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from '../redux/index.ts';
-console.log('store: ', store);
-
 dayjs.locale('zh-cn');
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <ConfigProvider locale={zhCN}>
+  <ConfigProvider locale={zhCN}>
+    {/* <Provider store={store}> */}
       <React.StrictMode>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </React.StrictMode>
-    </ConfigProvider>
-  </Provider>
+    {/* </Provider> */}
+  </ConfigProvider>
   ,
 )
