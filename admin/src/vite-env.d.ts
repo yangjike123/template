c1b8d8ef261241a2550d3b6d9bc82bb696d93fa5
yanjike <2711396853@qq.com>
2024-05-28 16:08:51 +0800

    配置环境类型文件

--- admin/src/vite-env.d.ts
+++ admin/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_APP_TITLE: string;
+    readonly VITE_REQUEST_URL: string;
+    readonly NODE_ENV: string;
+}
+declare interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
