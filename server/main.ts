9bc2c944708edc1052a2640c8e61bd3ea26b0754
yanjike <2711396853@qq.com>
2024-05-29 22:10:05 +0800

    跨域问题

--- server/main.ts
+++ server/main.ts
@@ -4,9 +4,7 @@ import varifyJwt from "./utils/verifyJwt";
 import config from "./config";
 import cookieParser from "cookie-parser";
 import session from "express-session";
-import cors from "cors";
 import { Sequelize } from "sequelize-typescript";
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -35,16 +33,22 @@ function initModels() {
 }
 initModels();
 
+// 允许跨域
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
-app.use(cors());// 允许跨域
+app.use(cookieParser()); // 解析cookie 设置
 app.use(express.json()); // 允许json传输格式
 app.use(express.urlencoded({ extended: false })); // 允许urlencoded传输格式
-app.use(cookieParser()); // cookie 设置
 app.use(session({
   secret: config.sessionSecretKey,  // 每一次在生成Cookie的时候，通过一个私钥生成一个字符串然后再交给客户端(读取我的配置文件里面的私钥key)
   resave: false,
-  saveUninitialized: true,
-  // cookie : {secure : true }
+  cookie: { httpOnly: true, maxAge: 60 * 1000 * 5 },
+  saveUninitialized: false,
 }));
 app.use(varifyJwt); // 验证token
 app.use(config.apiPrefix, index);// 接口路由
