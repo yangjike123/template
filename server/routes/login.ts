be11baac34d8af3d31c28fb56e5ed2f361e04e55
yanjike <2711396853@qq.com>
2024-05-29 22:10:50 +0800

    增加退出登录接口

--- server/routes/login.ts
+++ server/routes/login.ts
@@ -11,4 +11,7 @@ route.get('/code', serviceUser.getCode);
 // 用户信息
 route.get('/userinfo', serviceUser.getUserInfo);
 
+// 用户退出
+route.post('/logout', serviceUser.logout);
+
 export default route;
--- server/service/login/index.ts
+++ server/service/login/index.ts
@@ -1,26 +1,38 @@
 import { Request, Response } from 'express';
 import { HttpCode, HttpCodeMsg } from '../../../types/httpCode';
-import { IAccountLogin } from '../../../types/IAccount';
+import { IAccount, IAccountLogin } from '../../../types/IAccount';
 import config from '../../config';
 import jwt from 'jsonwebtoken';
 import svgCaptcha from 'svg-captcha';
-import { Session } from 'express-session';
 import AccountModel from '../../models/account';
-import { md5 } from '../../utils';
+import { exclude, md5 } from '../../utils';
 import RoleModel from '../../models/role';
 import DepartmentModel from '../../models/department';
 // 登录
-async function login({ body }: Request, res: Response) {
-    const { account, password } = body as IAccountLogin;
-    const data = await AccountModel.findOne({ where: { account }, include: [RoleModel, DepartmentModel] });
-    if (data.password === md5(password)) {
-        const token = jwt.sign({ userId: data.id }, config.jwtSecret, { expiresIn: '2d' });//存储时间为48小时
-        res.cookie(config.cookieName, token);
-        delete data.dataValues.password;
+const time = 1000 * 60 * 60 * 24 * 30;
+async function login(req: Request, res: Response) {
+    try {
+        const { account, password, code } = req.body as IAccountLogin;
+        const captcha: string = req.cookies['captcha'];
+        const data = await AccountModel.findOne({
+            where: { account },
+            include: [RoleModel, DepartmentModel],
+        });
+        if (data.password !== md5(password)) throw '密码错误';
+        if (!data.status) throw '账号已被禁用';
+        if (code.toLocaleLowerCase() !== captcha.toLocaleLowerCase()) throw '验证码错误';
+        const token = jwt.sign({ userId: data.id }, config.jwtSecret, { expiresIn: time });
+        res.cookie(config.cookieName, token, { maxAge: time, httpOnly: true, secure: true });
+        res.clearCookie('captcha');
         res.status(HttpCode.Ok).json({
             status: HttpCode.Ok,
             message: HttpCodeMsg.Ok,
-            data: { ...data.dataValues, token }
+            data: { ...exclude<IAccount>(data.dataValues, ['password']), token }
+        });
+    } catch (error) {
+        res.status(HttpCode.BadRequest).json({
+            status: HttpCode.BadRequest,
+            message: error,
         });
     }
 };
@@ -40,7 +52,7 @@ async function getUserInfo(req: Request, res: Response) {
 
 async function getCode(req: Request, res: Response) {
     const captcha = svgCaptcha.create({
-        size: 6, //验证码长度
+        size: 5, //验证码长度
         fontSize: 45, //验证码字号
         noise: 2, //干扰线条数目
         width: 100, //宽度
@@ -48,13 +60,19 @@ async function getCode(req: Request, res: Response) {
         color: true, //验证码字符是否有颜色，默认是没有，但是如果设置了背景颜色，那么默认就是有字符颜色
         background: '#ccc' //beijing
     });
-    (req.session as Session & { captcha: string }).captcha = captcha.text;
-    // console.log('captcha.text: ', captcha.text);
+    res.cookie('captcha', captcha.text, { maxAge: 60000, httpOnly: true, secure: true });
     res.status(HttpCode.Ok).json({
         status: HttpCode.Ok,
         message: HttpCodeMsg.Ok,
         data: captcha.data
     });
 }
-
-export default { login, getUserInfo, getCode };
+// 退出登录
+async function logout(req: Request, res: Response) {
+    res.clearCookie(config.cookieName, { httpOnly: true, secure: true });
+    res.status(HttpCode.Ok).json({
+        status: HttpCode.Ok,
+        message: HttpCodeMsg.Ok,
+    });
+}
+export default { login, getUserInfo, getCode, logout };
