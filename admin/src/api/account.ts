32a112d380c68fea100c7b506040d02f7e77abe7
yanjike <2711396853@qq.com>
2024-05-31 21:23:52 +0800

    账号接口api

--- admin/src/api/account.ts
+++ admin/src/api/account.ts
@@ -1,7 +1,27 @@
-import { IAccount, IAccountChangePassword } from "../../../types/IAccount";
-import { requsetPost } from "../../utils/request";
+import { IAccount, IAccountChangePassword, IAccountDetail, IAccountList, IAccountSearchParams, IAccountUpdate } from "../../../types/IAccount";
+import { requsetDelete, requsetGet, requsetPost, requsetPut } from "../../utils/request";
 
 // 修改用户密码
 export async function changePassword(data: IAccountChangePassword) {
     return requsetPost<{ data: IAccount }>('account/changePassword', data);
+}
+// 账户列表
+export async function getAccountList(params: IAccountSearchParams) {
+    return requsetGet<IAccountList>('account', params);
+}
+// 修改账户信息
+export async function updatedAccount(data: IAccountUpdate) {
+    return requsetPut<IAccountDetail>(`account/${data.id}`, data);
+}
+// 创建账户
+export async function createdAccount(data: IAccount) {
+    return requsetPost<IAccount>('account', data);
+}
+// 获取账户详情
+export async function getAccountDetail(id: number) {
+    return requsetGet<IAccountDetail>(`account/${id}`);
+}
+// 删除账户
+export async function deleteAccount(id: number) {
+    return requsetDelete<IAccountDetail>(`account/${id}`);
 }
