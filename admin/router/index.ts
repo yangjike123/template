15e3e80c1b795e6ca02c1c985cf00962840c4485
yanjike <2711396853@qq.com>
2024-06-01 14:35:23 +0800

    菜单路由数据分开

--- admin/router/index.ts
+++ admin/router/index.ts
@@ -1,61 +1,8 @@
-import { createElement, lazy } from 'react';
 import { IRouter } from './types';
-import * as icons from '@ant-design/icons';
-import { IRouters } from '../../types/IMenu';
+import defaultRouters from './defaultRouters';
 
-function Icon(props: { icon: string }) {
-    const { icon } = props;
-    const antIcon: { [key: string]: any } = icons;
-    return createElement(antIcon[icon]);
-};
+const baseUrl = '../src/pages/';
 
-const defaultRouters: IRouters[] = [
-    {
-        path: '/',
-        component: lazy(() => import('../src/pages/home')),
-        name: '首页',
-        icon: Icon({ icon: 'HomeOutlined' }),
-    },
-    {
-        path: '/system',
-        name: '系统管理',
-        icon: Icon({ icon: 'SettingOutlined' }),
-        children: [
-            {
-                name: '账号管理',
-                path: '/system/account',
-                icon: Icon({ icon: 'UserOutlined' }),
-                component: lazy(() => import('../src/pages/system/account')),
-            },
-            {
-                name: '角色管理',
-                path: '/system/role',
-                icon: Icon({ icon: 'UsergroupAddOutlined' }),
-                component: lazy(() => import('../src/pages/system/role')),
-            }
-        ]
-    },
-    {
-        path: 'userinfo',
-        name: '用户信息',
-        icon: Icon({ icon: 'UserOutlined' }),
-        component: lazy(() => import('../src/pages/userinfo')),
-        hideInMenu: true,
-    },
-    {
-        path: '/login',
-        component: lazy(() => import('../src/pages/login')),
-        name: '登录',
-        hideInMenu: true,
-    },
-    {
-        path: '*',
-        component: lazy(() => import('../src/404')),
-        name: '404',
-        hideInMenu: true,
-    }
-
-]
 
 const router: IRouter = {
     route: {
