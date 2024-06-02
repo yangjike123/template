97fec1b00da5e4c2512c017d360711101894f16e
yanjike <2711396853@qq.com>
2024-06-02 13:47:28 +0800

    增加 user对象

--- server/global.d.ts
+++ server/global.d.ts
@@ -1,11 +1,13 @@
 import { Request } from "express";
 import { Session } from "express-session";
+import { IAccount } from "../types/IAccount";
 
 // 这么在express Response对象里面添加一个新函数
 
 declare global {
     namespace Express {
         interface Request {
+            user: IAccount;
             userId: number;
         }
     }
