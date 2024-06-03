d6ef745234f25da0c5040a6a36bf867593747623
yanjike <2711396853@qq.com>
2024-06-03 11:19:59 +0800

    部门api

--- admin/src/api/department.ts
+++ admin/src/api/department.ts
@@ -1,5 +1,19 @@
-import { requestGet } from "../../utils/request";
+import { ISearchDepartmentParams, IGetDepartmentList, ICreateDepartment, IDeleteDepartment, IUpdateDepartment, IDepartmentDetail } from "../../../types/IDepartment";
+import { requestDelete, requestGet, requestPost, requestPut } from "../../utils/request";
 
-export function getDepartmentList() {
-  return requestGet('department');
+//部门列表
+export function getDepartmentList(params: ISearchDepartmentParams) {
+    return requestGet<IGetDepartmentList>('department', params);
+}
+// 创建部门
+export function createdDepartment(data: ICreateDepartment) {
+    return requestPost<IDepartmentDetail>('department', data);
+}
+// 更新部门
+export function updateDepartment(data: IUpdateDepartment) {
+    return requestPut<IDepartmentDetail>(`department/${data.id}`, data);
+}
+// 删除部门
+export function deleteDepartment(id: IDeleteDepartment['id']) {
+    return requestDelete(`department/${id}`);
 }
